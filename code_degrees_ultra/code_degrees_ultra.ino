b2d958ce669ea6a318e184434ceada0304784ed5
Ridiax <rickelmedev@gmail.com>
2020-10-28 17:23:26 -0300

    Adicionando o Ultrassonico e seu Calculador de Distancia

--- code_degrees_ultra/code_degrees_ultra.ino
+++ code_degrees_ultra/code_degrees_ultra.ino
@@ -2,8 +2,18 @@
 #include <Servo.h>
 Servo meuServo;
 
+// Adicionando variaveis do Ultrassonico
+const int trigPin = 2;
+const int echoPin = 3;
+long tempoDuracao;
+int distancia;
+
 void setup(){
+  // Servo
   meuServo.attach(9);
+  // Ultra
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
   
   Serial.begin(9600);
 }
@@ -13,13 +23,33 @@ void loop(){
   for (int r = 10; r <= 160; r++){
     meuServo.write(r);
     delay(35);
+    
+    distancia = calcularDistancia();
   }
 
   // Rotacionando ao contrario
   for (int r = 160; r > 10; r--){
     meuServo.write(r);
     delay(35);
+    
+    distancia = calcularDistancia();
   }
 
   
+}
+
+
+// Funcao para retornar a distancia em interio
+int calcularDistancia(){
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, LOW);
+
+  tempoDuracao  = pulseIn(echoPin, HIGH);
+  distancia     = tempoDuracao * 0.034/2;
+
+  return distancia;
 }
